7554e050620f8fb6b1e3c4a7ada7887ebeec85fa
mackenzie clarkson <maclark99@gmail.com>
2019-04-06 14:00:22 -0700

    onClick event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,29 @@ class Counter extends Component {
 		tags: ["tag1", "tag2", "tag3"]
 	};
 
+	render() {
+		// compiles to React.createElement
+		return (
+			//react.fragment seems to be like template in vue, prevents unnecessary additional elements
+			<div>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button
+					onClick={this.handleIncrement}
+					className="btn btn-secondary btn-sm">
+					Increment
+				</button>
+				{/* this works because javascript does need the two sides to be booleans, the second part is truthy */}
+				{/* and js returns the second operand */}
+				{this.state.tags.length === 0 && "Please Create a New Tag"}
+				{this.renderTags()}
+			</div>
+		);
+	}
+
+	handleIncrement() {
+		console.log("button clicked");
+	}
+
 	renderTags() {
 		if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
@@ -18,17 +41,16 @@ class Counter extends Component {
 		);
 	}
 
-	render() {
-		// compiles to React.createElement
-		return (
-			//react.fragment seems to be like template in vue, prevents unnecessary additional elements
-			<div>
-				{/* this works because javascript does need the two sides to be booleans, the second part is truthy */}
-				{/* and js returns the second operand */}
-				{this.state.tags.length === 0 && "Please Create a New Tag"}
-				{this.renderTags()}
-			</div>
-		);
+	getBadgeClasses() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		//jsx expressions are like normal js objects
+		return count === 0 ? "Zero" : count;
 	}
 }
 
