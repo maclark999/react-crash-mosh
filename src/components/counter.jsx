c4e1cd8df11e7ae5c75414c493d46200b69e02a6
mackenzie clarkson <maclark99@gmail.com>
2019-04-06 15:31:42 -0700

    passing props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
 	state = {
-		count: 0
+		value: this.props.value
 	};
 
 	render() {
+		console.log('props', this.props);
+		
 		// compiles to React.createElement
 		return (
 			//react.fragment seems to be like template in vue, prevents unnecessary additional elements
@@ -33,19 +35,19 @@ class Counter extends Component {
 	//I still think it looks cleaner this way though, so I'm going with it
 	handleIncrement = product => {
 		//have to explicitly tell react what has changed
-		this.setState({ count: this.state.count + 1});
+		this.setState({ value: this.state.value + 1});
 	}
 
 	getBadgeClasses() {
 		let classes = "badge m-2 badge-";
-		classes += this.state.count === 0 ? "warning" : "primary";
+		classes += this.state.value === 0 ? "warning" : "primary";
 		return classes;
 	}
 
 	formatCount() {
-		const { count } = this.state;
+		const { value } = this.state;
 		//jsx expressions are like normal js objects
-		return count === 0 ? "Zero" : count;
+		return value === 0 ? "Zero" : value;
 	}
 }
 
