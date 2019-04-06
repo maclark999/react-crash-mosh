8b139350863dbfcf8de03c24a5cf9401d0681ae3
mackenzie clarkson <maclark99@gmail.com>
2019-04-06 15:20:17 -0700

    add state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import Counter from './counter';
 
+
 class Counters extends Component {
-    state = {  }
-    render() { 
-        return (<div>
-            <Counter/>
-            <Counter/>
-            <Counter/>
-        </div>);
-    }
+	state = {
+		counters: [
+			{ id: 1, value: 0},
+			{ id: 2, value: 0},
+			{ id: 3, value: 0},
+			{ id: 4, value: 0},
+		]
+	}
+	state = {  }
+	render() { 
+		return (<div>
+			<Counter/>
+			<Counter/>
+			<Counter/>
+		</div>);
+	}
 }
  
 export default Counters;
