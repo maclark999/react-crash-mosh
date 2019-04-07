d4659f18940628cf08fb81585348a2d20c9878c3
mackenzie clarkson <maclark99@gmail.com>
2019-04-06 17:45:46 -0700

    destructuring props object

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,8 @@ class Counter extends Component {
 	// 	value: this.props.counter.value
 	// };
 
-	render() {		
+	render() {
+		const { onDelete, onIncrement, counter} = this.props;
 		// compiles to React.createElement
 		return (
 			//react.fragment seems to be like template in vue, prevents unnecessary additional elements
@@ -13,12 +14,12 @@ class Counter extends Component {
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button
 					// needs to be an arrow function because passing an argument
-					onClick={() => this.props.onIncrement(this.props.counter)}
+					onClick={() => onIncrement(counter)}
 					className="btn btn-secondary btn-sm">
 					Increment
 				</button>
 				<button
-					onClick={() => this.props.onDelete(this.props.counter.id)}
+					onClick={() => onDelete(counter.id)}
 					className="btn btn-danger btn-sm m-2">
 					Delete
 				</button>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 //no state, no methods => stateless functional component
 //react passes props as argument at runtime
-const Navbar = props => {
+//also, destructuring!
+const Navbar = ({ totalCounters }) => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<span className="navbar-brand mb-0 h1">Total Counters:
 				<span className="badge badge-pill badge-secondary m-2">
-					{props.totalCounters}
+					{totalCounters}
 				</span>
 			</span>
 		</nav>
